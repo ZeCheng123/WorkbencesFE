916bce11600e9fdeb19d9d0eee1c547b36ba0a75
xiaofeng1liu <932534229@qq.com>
2024-04-09 11:38:44 +0800

    任务接口及新增派工单对接

--- src/api/common.js
+++ src/api/common.js
@@ -11,3 +11,17 @@ export const getLoginInfo = (data) => {
     url: '/md/api/task/list'
   })
 }
+export const getFieldJob = (data) => {
+  return request({
+    method: 'GET',
+    url: '/md/api/field-job?id='+data
+  })
+}
+
+export const addFieldJob = (data) => {
+  return request({
+    method: 'POST',
+    data:data,
+    url: '/md/api/field-job'
+  })
+}

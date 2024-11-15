c31902655952d857d3881e655e5153aa00b4d6a2
Harley.Huang <Harley.Huang@kwe.com.cn>
2024-11-15 10:13:52 +0800

    test

--- dist/assets/userinfo-B4NFwAcm.js
+++
@@ -1,1 +0,0 @@
-import{H as s}from"./index-DwwqDRnZ.js";const r=s({id:"userinfo",state:()=>({userinfo:{}}),actions:{getUserInfo(){return this.userinfo},setUserInfo(e){this.userinfo=e}}});export{r as u};
---
+++ dist/assets/userinfo-CbCawm5q.js
@@ -0,0 +1,1 @@
+import{D as s}from"./index-BNw8j_YZ.js";const r=s({id:"userinfo",state:()=>({userinfo:{}}),actions:{getUserInfo(){return this.userinfo},setUserInfo(e){this.userinfo=e}}});export{r as u};
